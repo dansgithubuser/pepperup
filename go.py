76f3bb5c7d9c02091ea3637acc7515fcc5a9779e
Dan <dansonlinepresence@gmail.com>
2018-07-01 12:36:14 -0400

    go.py

---
+++ go.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+def invoke(invocation, path='.'):
+	import os, subprocess
+	start=os.getcwd()
+	os.chdir(path)
+	subprocess.check_call(invocation, shell=True)
+	os.chdir(start)
+
+import argparse
+parser=argparse.ArgumentParser()
+parser.add_argument('--build', '-b', action='store_true')
+parser.add_argument('--run', '-r', action='store_true')
+args=parser.parse_args()
+
+if args.build:
+	invoke('mix deps.get')
+	invoke('mix deps.compile')
+
+if args.run:
+	invoke('mix phx.server')
